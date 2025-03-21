ea8eeb59e44a100caf3c8451755545d0b46b6f67
NxtPerfect <alakaxan@protonmail.com>
2025-03-21 10:48:22 +0100

    Fixing files again

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-=======
 const config = {
   plugins: ["@tailwindcss/postcss"],
->>>>>>> version2
 };
 
 export default config;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,4 @@
 import type { Metadata } from "next";
-<<<<<<< HEAD
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "./navbar";
-import Footer from "./footer";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Aurora Portfolio",
-  description: "Portfolio of frontend web developer.",
-=======
 import { Playfair_Display } from "next/font/google";
 import "./globals.css";
 
@@ -22,7 +10,6 @@ const playfair = Playfair_Display({
 export const metadata: Metadata = {
   title: "Hazel's Portfolio",
   description: "Simple, minimalistic, to the point.",
->>>>>>> version2
 };
 
 export default function RootLayout({
@@ -31,22 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-<<<<<<< HEAD
-    <html lang="en" className="scroll-smooth scroll-p-[45%]">
-      <body className={inter.className + ""}>
-        <Navbar />
-        <div className="absolute w-full h-full bg-[url('/nnnoise.svg')] bg-repeat bg-neutral-900 opacity-15 contrast-150 brightness-[3]"></div>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-neutral-900">
-          {children}
-        </main>
-        <Footer />
-=======
     <html lang="en">
       <body
         className={`${playfair.className} antialiased`}
       >
         {children}
->>>>>>> version2
       </body>
     </html>
   );
